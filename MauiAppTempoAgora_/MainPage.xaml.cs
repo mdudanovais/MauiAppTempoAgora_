b38331491f478cac248d540ef766513bffea32b6
mdudanovais <diana-novais@hotmail.com>
2025-04-06 00:24:31 -0300

    Agenda 07 parcialmente concluída

--- MauiAppTempoAgora_/MainPage.xaml.cs
+++ MauiAppTempoAgora_/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace MauiAppTempoAgora_
+﻿using MauiAppTempoAgora_.Models;
+using MauiAppTempoAgora_.Services;
+
+namespace MauiAppTempoAgora_
 {
     public partial class MainPage : ContentPage
     {
@@ -9,16 +12,51 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        
+
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                    if(t != null)
+                    {
+                        string dados_previsao = "";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                        $"Longitude: {t.lon} \n" +
+                                        $"Temperatura Minima: {t.temp_min} \n" +
+                                        $"Temperatura Maxima: {t.temp_max} \n" +
+                                        $"Descrição: {t.description} \n" +
+                                        $"Main: {t.main} \n" +
+                                        $"Velocidade do Vento: {t.speed} \n" +
+                                        $"Visibilidade: {t.visibility} \n" +
+                                        $"Nascer do Sol: {t.sunrise} \n" +
+                                        $"Pôr do Sol: {t.sunset} \n";
+
+                        lbl_res.Text = dados_previsao;
+
+
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Sem dados de Previsão";
+                    }
+
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+                }
+
+            }
+            catch(Exception ex)
+            {
+              await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
     }
 
