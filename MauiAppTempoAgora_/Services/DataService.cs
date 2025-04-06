7dfba0c3cc5a9528e4598c72e1fe5ad379d684bc
mdudanovais <diana-novais@hotmail.com>
2025-04-06 16:28:35 -0300

    Tratamento de erros.

--- MauiAppTempoAgora_/Services/DataService.cs
+++ MauiAppTempoAgora_/Services/DataService.cs
@@ -1,5 +1,6 @@
 ﻿using MauiAppTempoAgora_.Models;
 using Newtonsoft.Json.Linq;
+using System.Net;
 
 namespace MauiAppTempoAgora_.Services
 {
@@ -15,37 +16,56 @@ namespace MauiAppTempoAgora_.Services
 
             using (HttpClient client = new HttpClient())
             {
-                HttpResponseMessage resp = await client.GetAsync(url);
-
-                if (resp.IsSuccessStatusCode)
+                try
                 {
-                    string json = await resp.Content.ReadAsStringAsync();
 
-                    var rascunho = JObject.Parse(json);
+                    HttpResponseMessage resp = await client.GetAsync(url);
 
-                    DateTime time = new();
-                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
-                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
-
-
-                    t = new()
+                    if (resp.StatusCode == HttpStatusCode.NotFound)
                     {
-                        lat = (double)rascunho["coord"]["lat"],
-                        lon = (double)rascunho["coord"]["lon"],
-                        description = (string)rascunho["weather"][0]["description"],
-                        main = (string)rascunho["weather"][0]["main"],
-                        temp_min = (double)rascunho["main"]["temp_min"],
-                        temp_max = (double)rascunho["main"]["temp_max"],
-                        speed = (double)rascunho["wind"]["speed"],
-                        visibility = (int)rascunho["visibility"],
-                        sunrise = sunrise.ToString(),
-                        sunset = sunset.ToString(),
-                    }; // Fecha obj do Tempo
-                } // Fecha if se o status do servidor foi de sucesso
-            }// Fecha laço using
-                 
+                        throw new Exception("Cidade não encontrada, digite novamente.");
+                        // Mensagem específica para quando a cidade não for encontrada.
+
+                    }
+
+                    else if (resp.IsSuccessStatusCode)
+                    {
+                        string json = await resp.Content.ReadAsStringAsync();
+
+                        var rascunho = JObject.Parse(json);
+
+                        DateTime time = new();
+                        DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                        DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
 
 
+                        t = new()
+                        {
+                            lat = (double)rascunho["coord"]["lat"],
+                            lon = (double)rascunho["coord"]["lon"],
+                            description = (string)rascunho["weather"][0]["description"],
+                            main = (string)rascunho["weather"][0]["main"],
+                            temp_min = (double)rascunho["main"]["temp_min"],
+                            temp_max = (double)rascunho["main"]["temp_max"],
+                            speed = (double)rascunho["wind"]["speed"],
+                            visibility = (int)rascunho["visibility"],
+                            sunrise = sunrise.ToString(),
+                            sunset = sunset.ToString(),
+                        }; // Fecha obj do Tempo.
+                    } // Fecha if se o status do servidor foi de sucesso.
+                }
+                catch (HttpRequestException ex)
+                {
+                    throw new Exception("Erro ao acessar o servidor, tente novamente mais tarde.");
+                    // Mensagem específica para quando houver erro de conexão com o servidor.
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Erro inesperado: " + ex.Message);
+                    // Mensagem genérica para outros erros.
+                }
+
+            } // Fecha o using.
                 return t;
         }
     }
